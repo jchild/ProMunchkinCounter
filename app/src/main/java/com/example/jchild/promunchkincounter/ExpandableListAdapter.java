24878282563235034be0c39ea40a4a19edb5ae40
Jonathan Child <jchild45@gmail.com>
2015-07-10 17:47:40 +0800

    added rules, changed app save location
    +more rules
    +changed the default install location to allow user to save app to SD
    card if one is present.
    -removed unused .ttf file

--- app/src/main/java/com/example/jchild/promunchkincounter/ExpandableListAdapter.java
+++ app/src/main/java/com/example/jchild/promunchkincounter/ExpandableListAdapter.java
@@ -19,10 +19,10 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
     private Context _context;
     private List<String> _listDataHeader; // header titles
     // child data in format of header title, child title
-    private HashMap<String, List<String>> _listDataChild;
+    private HashMap<String, String> _listDataChild;
 
     public ExpandableListAdapter(Context context, List<String> listDataHeader,
-                                 HashMap<String, List<String>> listChildData) {
+                                 HashMap<String, String> listChildData) {
         this._context = context;
         this._listDataHeader = listDataHeader;
         this._listDataChild = listChildData;
@@ -30,8 +30,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public Object getChild(int groupPosition, int childPosititon) {
-        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
-                .get(childPosititon);
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition));
     }
 
     @Override
@@ -60,8 +59,7 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
-                .size();
+        return 1;
     }
 
     @Override
--- app/src/main/java/com/example/jchild/promunchkincounter/Rules.java
+++ app/src/main/java/com/example/jchild/promunchkincounter/Rules.java
@@ -17,7 +17,7 @@ public class Rules extends ActionBarActivity {
     ExpandableListAdapter listAdapter;
     ExpandableListView expListView;
     List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
+    HashMap<String, String> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +64,7 @@ public class Rules extends ActionBarActivity {
 
     private void prepareListData() {
         listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
+        listDataChild = new HashMap<String, String>();
 
         // Adding child data
         listDataHeader.add("Conflicts Between Cards and Rules");
@@ -74,25 +74,41 @@ public class Rules extends ActionBarActivity {
         listDataHeader.add("When You May Take Actions");
 
         // Adding child data
-        List<String> rule1 = new ArrayList<String>();
-        rule1.add("\tThe Rulesheet gives the general rules. Many cards add special rules, so in most cases when the rulesheet disagrees with a car, follow the card. However, ignore any card effect that might seem to contradict one of the rules listed below unless the card explicitly says it supersedes that rule!" +
+        String rule0 = ("\tThe Rulesheet gives the general rules. Many cards add special rules, so in most cases when the rulesheet disagrees with a car, follow the card. However, ignore any card effect that might seem to contradict one of the rules listed below unless the card explicitly says it supersedes that rule!" +
                 "\n \n 1. Nothing can reduce a player below Level 1, altough card effects might reduce a player's or a monster's combat strength below 1. \n\n 2. You go up a level after combat only if you kill a monster. \n 3. You cannot collect rewards for defeating a monster(e.g., Treasure, levels) in the middle of a combat. You must finish the fight before gaining any rewards." +
                 "\n \n 4. You must kill a monster to reach Level 10.\n\n\t Any other disputes should be settled by loud arguments, with the owner of the game having the last word.");
 
 
-        List<String> rule2 = new ArrayList<String>();
-        rule2.add("\tKeep separate face-up discard piles for the two decks. You may not look through the discards unless you play a card that allows you to!" +
+        String rule1 = ("\tKeep separate face-up discard piles for the two decks. You may not look through the discards unless you play a card that allows you to!" +
                 "\n\n\tWhen a deck runs out, reshuffle its discards. If a deck runs out and there are no discards, nobody can draw any of that kind of card!" +
                 "\n\n\tIn Play: These are the cards on the table in front of you, showing your Race and Class (if any) and the Items you are carrying. Continuing Courses and some other cards also stay on the table after you play them. Cards in play are public information and must be visible to the other players." +
                 "\n\n\tYour Hand: Cards in your hand ar not in play. They don't help you, but they can't be taken away except by cards that specifically affect \"your hand\". At the end of your turn, you may have no more than five cards in your hand." +
                 "\n\n\tCards in play must not be returned to your hand - they must be discarded or traded if you want to get rid of them.");
 
-        List<String> rule3 = new ArrayList<String>();
-        rule3.add("\tEveryone Starts as a Level 1 human with no class. (Heh, heh.) Munchkin characters are exclusively male or female, and your character's sex is he same as your own unless you declare otherwise." +
+        String rule2 = ("\tEveryone Starts as a Level 1 human with no class. (Heh, heh.) Munchkin characters are exclusively male or female, and your character's sex is he same as your own unless you declare otherwise." +
                 "\n\n\tLook at your initial eight cards. if you have any Race or Class cards, you may (if you like) play one of each type by placing it in front of you. If you have any usable Items, you may play them by placing them in front of you. If you have any doubt about whether you should play a card, you could read below, or you could just charge ahead and do it.");
 
-        listDataChild.put(listDataHeader.get(0), rule1); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), rule2);
-        listDataChild.put(listDataHeader.get(2), rule3);
+        String rule3 = ("\tDecide who goes first by rolling the dice and arguing about the results and the meaning of this sentence and whether the fact that a word seems to be missing any effect." +
+                "\n\n\tPlay proceeds in turns, each with several phases (see phases). When the first player finishes his turn, the player to his left takes a turn, and so on." +
+                "\n\n\tThe first player to reach Level 10 wins... but you must reach Level 10 by killing a  monster, unless a card specifically  allows you to win another way.");
+
+        String rule4 = ("\tYou may perform these actions at any time:" +
+                "\n\t-Discard a Class or Race." +
+                "\n\t-Play a Go Up a Level or Hireling." +
+                "\n\t-Play a Curse." +
+                "\n\n\tYou May perform these actions at any time, as long as you are not in combat:" +
+                "\n\t-Trade an Item with another player (the other player may not be in combat, either)." +
+                "\n\t-Change which Items you have equipped." +
+                "\n\t-Play a card that you have just received (some cards may be played even during combat)." +
+                "\n\n\tYou may perform these actions on your own turn:" +
+                "\n\t-Play a new class or Race card (at any time)." +
+                "\n\t-Sell Items for levels (except when you are in combat)." +
+                "\n\t-Play an Item (most Items cannot be played during combat, but some one-shot Items can;[See One-Shot Treasures]).");
+
+        listDataChild.put(listDataHeader.get(0), rule0); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), rule1);
+        listDataChild.put(listDataHeader.get(2), rule2);
+        listDataChild.put(listDataHeader.get(3), rule3);
+        listDataChild.put(listDataHeader.get(4), rule4);
     }
 }
